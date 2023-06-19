e855ef73cac44b3b7c4eeccf24bf239d5b63ad78
Ollie-Judge <ollie.judge@hotmail.com>
2023-06-19 15:24:26 +0100

    made the footer overlap the background

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,56 +1,85 @@
-import { Typography, Grid, Box } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import footerBackground from "../images/layered-waves-haikei.svg";
 import "./style/footer.css";
 
 function Footer() {
   return (
-    <Box
-      id="footerContainer"
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
       style={{
         width: "100%",
         margin: "auto",
         padding: "0",
+        position: "relative",
+        bottom: "0",
+        background: "transparent",
       }}
-      className="backgroundFooterColour"
     >
       <Grid
-        container
-        direction="column"
-        alignItems="center"
+        item
+        xs={12}
         style={{
+          bottom: "-200px",
+          zindex: "2",
           position: "relative",
-          bottom: "0",
-          background: "transparent",
         }}
       >
+        <FontAwesomeIcon icon={faCoffee} />
+      </Grid>
+
+      <Grid container>
         <Grid
           item
-          xs={12}
+          spacing={3}
           style={{
-            margin: "50px 25px auto",
             zindex: "2",
+            margin: "auto",
             position: "relative",
+            bottom: "-200px",
           }}
         >
-          <Typography variant="h2">Footer</Typography>
+          <Typography>Footer</Typography>
         </Grid>
 
-        <Grid item>
-          <Typography style={{ zindex: "2", position: "relative" }}>
-            Footer
-          </Typography>
-        </Grid>
-        <img
-          src={footerBackground}
-          alt="footer background"
+        <Grid
+          item
+          spacing={3}
           style={{
-            zindex: "-1",
-            position: "absolute",
-            width: "100%",
+            margin: "auto",
+            zindex: "2",
+            position: "relative",
+            bottom: "-200px",
           }}
-        ></img>
+        >
+          <Typography>Footer</Typography>
+        </Grid>
+
+        <Grid
+          item
+          spacing={3}
+          style={{
+            margin: "auto",
+            zindex: "2",
+            position: "relative",
+            bottom: "-200px",
+          }}
+        >
+          <Typography>Footer</Typography>
+        </Grid>
       </Grid>
-    </Box>
+      <img
+        src={footerBackground}
+        alt="footer background"
+        style={{
+          zindex: "-1",
+          width: "100%",
+        }}
+      ></img>
+    </Grid>
   );
 }
 
